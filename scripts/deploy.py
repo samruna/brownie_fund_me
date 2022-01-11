aebda6d5aae1289e47096046505999afd3a21eaa
samruna <samay77@gmail.com>
2022-01-10 18:24:32 -0800

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,33 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+    print("Account acquired")
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+        print("Not local BC")
+    else:
+        print("Deploying mock")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    print("Deploying fundme")
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
